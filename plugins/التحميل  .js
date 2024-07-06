44f146c80f2c2d05bdfcf11f8be10859f1d6206e
Kakashi6664 <6112007mo@gmail.com>
2024-07-06 23:18:08 +0300

    Update التحميل  .js

--- plugins/التحميل  .js
+++ plugins/التحميل  .js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/ef2b22a623f982f249b77.jpg';
+let videoUrl = 'https://telegra.ph/file/7e1e33ce31658bc192c32.jpg';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
