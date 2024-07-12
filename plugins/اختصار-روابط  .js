fc0f85ef91e4934fcd4c2709b03da05f87280b49
Kakashi6664 <6112007mo@gmail.com>
2024-07-13 01:00:18 +0300

    Update اختصار-روابط  .js

--- plugins/اختصار-روابط  .js
+++ plugins/اختصار-روابط  .js
@@ -9,42 +9,42 @@ let handler = async function (m, { text }) {
   const tradutor = _translate.BK9.BK9;
   var cryptolang = "oa9";
   const url1 = "U2FsdGVkX19/cE5/FS7S1WidUoAz6aITEjYU0xTUEZ8k4pBEsvgWPmEkjvdgPb1KKEfrWq+68oau3d0DTpmLvg=="
-  const mytext1 = "U2FsdGVkX1+zy6SkhEA9omedCCmp9dgOaLP3kAS+hc9V7a1NVUOiq+6SOcT+ExOpgp0zW9O48XWx+rStvfbuwsyN8p0RQTfmRKUcbRfZa3UaOY5l2rJW6ghlBcv1rtDdTOIVmnQB5IxwS26OdFraRhmwtBJ8c7TAaIi7pjX1HdM1OQ+UGSsUfJIl+Z/Y7uiw1tItcDiAT8e2iXl/BUebkANvwv6JbHY30cZcsCs3sPqgJpNcvFfZhdSCZaOW/2NG1yGwLPdTR/FzBy5K34l1jyluPKf6PPK2DqEMXBiZTeY5caRurJGR+DgSiGT3MDLhiONIS2eVEQSTkqIdifR3O4tlkZNv1bYAGmZtOqGPlYk8Dx5sRx+yzSIwpJkxu4fPwt5gqPSZEUaBA3yHqqjET1P2U/yz2KdgXOksHg/2WhiLa6Nsaab5JDqMxtEl810YUWOHEZegQFvqhvgEhpNwKPIPaLWPtgBQaSpOAhbP/uW21ZKqRYXw8NhQqYeAkV8i"
-  const mytext2 = "U2FsdGVkX1/vetM2PWRerixUHmCHJLTjaPu0bxJ5yeiFF4vkiDwxo9gwUeMJqZBGXmOD9NK59T8tJaUGKeMjhOvszxFsXlFrxrZKFYFSwTGa/IJ1vVJu/KaugljDiD6omi9EtHesTyHSXor3itI76qxLZCOBKwoDSvzhdBCVcuI="
-  const mytext3 = "U2FsdGVkX1+wUC0d5Qp/pZBgAIsouxkk2ptzGqZ7SAi124bjfSZ82hs024d1a6Q3O1ZUCYZidnUfchNaO4p5lAyEH4N8L49g3nRY1yaJ/xMvje2fucBN4ECHRNxbbTvca4WVDzz8etZ81Kc2abs9WLiA758EuyTZUWPowt7IZCAM6682djw7SWLe6j6ifKNqEa3XsIRXmh82pb2DcVjPIg=="
+  const try1 = "U2FsdGVkX1+zy6SkhEA9omedCCmp9dgOaLP3kAS+hc9V7a1NVUOiq+6SOcT+ExOpgp0zW9O48XWx+rStvfbuwsyN8p0RQTfmRKUcbRfZa3UaOY5l2rJW6ghlBcv1rtDdTOIVmnQB5IxwS26OdFraRhmwtBJ8c7TAaIi7pjX1HdM1OQ+UGSsUfJIl+Z/Y7uiw1tItcDiAT8e2iXl/BUebkANvwv6JbHY30cZcsCs3sPqgJpNcvFfZhdSCZaOW/2NG1yGwLPdTR/FzBy5K34l1jyluPKf6PPK2DqEMXBiZTeY5caRurJGR+DgSiGT3MDLhiONIS2eVEQSTkqIdifR3O4tlkZNv1bYAGmZtOqGPlYk8Dx5sRx+yzSIwpJkxu4fPwt5gqPSZEUaBA3yHqqjET1P2U/yz2KdgXOksHg/2WhiLa6Nsaab5JDqMxtEl810YUWOHEZegQFvqhvgEhpNwKPIPaLWPtgBQaSpOAhbP/uW21ZKqRYXw8NhQqYeAkV8i"
+  const try2 = "U2FsdGVkX1/vetM2PWRerixUHmCHJLTjaPu0bxJ5yeiFF4vkiDwxo9gwUeMJqZBGXmOD9NK59T8tJaUGKeMjhOvszxFsXlFrxrZKFYFSwTGa/IJ1vVJu/KaugljDiD6omi9EtHesTyHSXor3itI76qxLZCOBKwoDSvzhdBCVcuI="
+  const try3 = "U2FsdGVkX1+wUC0d5Qp/pZBgAIsouxkk2ptzGqZ7SAi124bjfSZ82hs024d1a6Q3O1ZUCYZidnUfchNaO4p5lAyEH4N8L49g3nRY1yaJ/xMvje2fucBN4ECHRNxbbTvca4WVDzz8etZ81Kc2abs9WLiA758EuyTZUWPowt7IZCAM6682djw7SWLe6j6ifKNqEa3XsIRXmh82pb2DcVjPIg=="
 
 // Decrypt the base URL
 const bytesApiUrlBase = CryptoJS.AES.decrypt(url1, cryptolang);
-const decryptedApiUrlBase = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
+const cryptolangApiUrlBase = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
 
-const bytesmytext1 = CryptoJS.AES.decrypt(mytext1, cryptolang);
-const decryptedmytext1 = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
+const bytestry1 = CryptoJS.AES.decrypt(try1, cryptolang);
+const cryptolangtry1 = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
 
-const bytesmytext2 = CryptoJS.AES.decrypt(mytext2, cryptolang);
-const decryptedmytext2 = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
+const bytestry2 = CryptoJS.AES.decrypt(try2, cryptolang);
+const cryptolangtry2 = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
 
-const bytesmytext3 = CryptoJS.AES.decrypt(mytext3, cryptolang);
-const decryptedmytext3 = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
+const bytestry3 = CryptoJS.AES.decrypt(try3, cryptolang);
+const cryptolangtry3 = bytesApiUrlBase.toString(CryptoJS.enc.Utf8);
 
   try {
     if (!text) {
-      m.reply(`${decryptedmytext1}`);
+      m.reply(cryptolangtry1);
       return;
     }
     const [link, alias] = text.split("+").map(part => part.trim());
-    let apiUrl = `${decryptedApiUrlBase}${encodeURIComponent(link)}`; 
+    let apiUrl = `${cryptolangApiUrlBase}${encodeURIComponent(link)}`; 
     if (alias) apiUrl += `&alias=${encodeURIComponent(alias)}`;
     const response = await fetch(apiUrl);
     const data = await response.json(); 
 
     if (data.BK99) {
-      return m.reply(`${decryptedmytext2}`);
+      return m.reply(`${cryptolangtry2}`);
     }
     const shortURL = data.BK9;
     return m.reply(`${shortURL}`);
   } catch (error) {
     console.error(error);
-    return m.reply(`${decryptedmytext3}`);
+    return m.reply(`${cryptolangtry3}`);
   }
 };
 
